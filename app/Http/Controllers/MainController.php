a7bbe68bab1718adbac694bbfad689a3ae742c6f
Franklyn Viana dos Santos <franklyn_vs_@hotmail.com>
2025-07-08 13:20:02 -0300

    Enhance exercise generation logic to include random operations and validate number_one against number_two

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -19,7 +19,7 @@ class MainController extends Controller
             'check_subtraction' => 'required_without_all:check_sum,check_multiplication,check_division',
             'check_multiplication' => 'required_without_all:check_sum,check_subtraction,check_division',
             'check_division' => 'required_without_all:check_sum,check_subtraction,check_multiplication',
-            'number_one' => 'required|integer|min:1|max:999',
+            'number_one' => 'required|integer|min:1|max:999|lt:number_two',
             'number_two' => 'required|integer|min:1|max:999',
             'number_exercises' => 'required|integer|min:5|max:100',
         ], [
@@ -31,6 +31,7 @@ class MainController extends Controller
             'number_one.integer' => 'O campo mínimo deve ser um número inteiro.',
             'number_one.min' => 'O campo mínimo deve ser pelo menos 1.',
             'number_one.max' => 'O campo mínimo não pode ser maior que 999.',
+            'number_one.lt' => 'O campo mínimo deve ser menor que o campo máximo.',
             'number_two.required' => 'O campo máximo é obrigatório.',
             'number_two.integer' => 'O campo máximo deve ser um número inteiro.',
             'number_two.min' => 'O campo máximo deve ser pelo menos 1.',
@@ -41,7 +42,56 @@ class MainController extends Controller
             'number_exercises.max' => 'O número de exercícios não pode ser maior que 100.',
         ]);
 
-        dd($request->all());
+        $operations = [];
+        $operations[] = $request->check_sum ? 'sum' : null;
+        $operations[] = $request->check_subtraction ? 'subtraction' : null;
+        $operations[] = $request->check_multiplication ? 'multiplication' : null;
+        $operations[] = $request->check_division ? 'division' : null;
+
+        $min = $request->number_one;
+        $max = $request->number_two;
+
+        $numberExercises = $request->number_exercises;
+
+        $exercises = [];
+        for ($index = 0; $index < $numberExercises; $index++) {
+            $operation = $operations[array_rand($operations)];
+            $number1 = rand($min, $max);
+            $number2 = rand($min, $max);
+
+            $exercise = '';
+            $solution = '';
+
+            switch ($operation) {
+                case 'sum':
+                    $exercise = "$number1 + $number2 = ";
+                    $solution = $number1 + $number2;
+                    break;
+                case 'subtraction':
+                    $exercise = "$number1 - $number2 = ";
+                    $solution = $number1 - $number2;
+                    break;
+                case 'multiplication':
+                    $exercise = "$number1 * $number2 = ";
+                    $solution = $number1 * $number2;
+                    break;
+                case 'division':
+                    if ($number2 == 0) {
+                        $number2 = 1;
+                    }
+                    $exercise = "$number1 / $number2 = ";
+                    $solution = $number1 / $number2;
+                    break;
+            }
+
+            $exercises[] = [
+                'exercise_number' => $index + 1,
+                'exercise' => $exercise,
+                'solution' => "$exercise $solution",
+            ];
+        }
+
+        dd($exercises);
     }
 
     public function printExercises(): void
